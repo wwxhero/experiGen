4c4ecd0706b93239c3b77389cf4d9343a0de1b5a
wanxwang <wanxin-wang@uiowa.edu>
2021-11-16 17:32:39 -0600

    CC SC_A_csv_script.m

--- SC_A_csv_script.m
+++ SC_A_csv_script.m
@@ -1,5 +1,5 @@
-path_input = './SC_V/SC_design_V_3-5.csv';
-path_output = './SC_V/SC_design_V_3-5_full';
+path_input = './SC_A/SC_design_A_3-5.csv';
+path_output = './SC_A/SC_design_A_3-5_full';
 n_recordsPerOutput = 20;
 
 % end of configuration specification
@@ -15,27 +15,27 @@ X = table2struct(X_table);
 
 for i_X = n_X:-1:1
 	Y(i_X).trialNum = i_X;
-	Y(i_X).trialName = sprintf('%sx%gx%sx%dx%sx%sx%s' ...
+	Y(i_X).trialName = sprintf('%sx%sx%gx%sx%gx%gx%s' ...
 					, X(i_X).ModalityCondition ...
 					, X(i_X).TTCv_s_ ...
 					, X(i_X).TTCa_s_ ...
 					, X(i_X).vV_km_h_ ...
+					, X(i_X).vA_km_h_ ...
 					, X(i_X).SoundLevel_dB_ ...
-					, X(i_X).TTCa_s_ ...
 					, veh.objType(X(i_X).vehicleSize));
 	Y(i_X).corrAns = 0;
 	Y(i_X).playSound = 'FALSE';
-	Y(i_X).objNum = 1;
-	Y(i_X).objType = veh.objType(X(i_X).vehicleSize);
-	Y(i_X).gain = 'NA';
+	Y(i_X).objNum = 2;
+	Y(i_X).objType = 'Audio';
+	Y(i_X).gain = X(i_X).SoundLevel_dB_;
 	Y(i_X).customMot = 'FALSE';
 	Y(i_X).customFile = '""';
 	Y(i_X).customDur = 0;
-	Y(i_X).objScale = veh.objScale_csv(X(i_X).vehicleSize);
-	Y(i_X).objRot = veh.objRot_csv(X(i_X).vehicleSize);
-	Y(i_X).startPos = sprintf('"%g,0,2.8288"', -(X(i_X).TTCv_s_)*X(i_X).vV_km_h_*0.277778);
+	Y(i_X).objScale = '"1, 1, 1"';
+	Y(i_X).objRot = '"0, 90, 0"';
+	Y(i_X).startPos = sprintf('"%g,0,2.8288"', -(X(i_X).TTCa_s_)*X(i_X).vA_km_h_*0.277778);
 	Y(i_X).endPos = '"0,0,2.8288"';
-	Y(i_X).velocity = X(i_X).vV_km_h_*0.277778;
+	Y(i_X).velocity = X(i_X).vA_km_h_*0.277778;
 	Y(i_X).timeVisible = X(i_X).TTCv_s_;
 	Y(i_X).rotationSpeedX = 0;
 	Y(i_X).rotationSpeedY = 0;
